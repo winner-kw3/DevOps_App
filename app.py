6a1457ab59ec9d2cd5574b1dbe25ebe1dccb2f4d
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2025-05-15 16:58:12 +0200

    Update

--- app.py
+++ app.py
@@ -99,5 +99,4 @@ def recommendations():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
